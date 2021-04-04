d06afd0419263017fd778a76a687652e5d040ff0
lime_train <me@liamstevens.me>
2021-04-04 21:39:54 +1000

    completed db creation function

--- src/state_manager.py
+++ src/state_manager.py
@@ -15,10 +15,15 @@ dbpass = (open(dbcreds,'r').read())
 def connect_db(db='postgres'):
     dbconn = postgresql.open(f'pg://{dbuser}:{dbpass}@{dbhost}:{dbport}/{db}'
     return dbconn
+
 def create_db(dbname='trade',dbconn):
     dbconn.execute('CREATE DATABASE '+dbname+' OWNER'+dbuser+';')
-    dbconn.execute('CREATE SCHEMA cryptoml')
-    dbconn.execute('CREATE TABLE state ( guid, datetime, accountID, nodeID, numWorker, walletstate, config)')
-    
+    dbconn.execute('CREATE SCHEMA cryptoml;')
+    dbconn.execute('CREATE TABLE state ( guid, datetime, accountID, nodeID, num_worker, wallet_state, config);')
+    dbconn.execute('CREATE TABLE transaction ( guid, creation_time, source, destination, vol_s, vol_d, complete,complete_time, stateguid );')
+    dbconn.execute('CREATE TABLE exchange_creds ( guid, config_guid, accesskey, accessID );')
+    dbconn.execute('CREATE TABLE node_configuration ( guid, config_guid, strategy, nodeID, start_time );')
+    dbconn.execute('CREATE TABLE configuration ( guid, base_investment, state_guid, nodeconf_guid, accountID, creds_guid, last_update, realise_value, realise_target );')
+    dbconn.execute('CREATE TABLE wallet_state ( guid, exchange, coin, volume, state_guid );')
 
 def create_schema():
