c5044916046fc521ded755077d2d19a50bb85c06
lime_train <me@liamstevens.me>
2021-04-19 22:57:24 +1000

    Migrated functions into class proper, added docstrings

--- src/state_manager.py
+++ src/state_manager.py
@@ -22,48 +22,70 @@ addr = (localhost, port)
 stateserver = socket(AF_INET, SOCK_STREAM)
 stateserver.bind(addr)
 
-'''
-Returns a database connection object for the operational database.
-
-@return:
-    dbconn: a database connection object
-'''
-def connect_db():
-    dbconn = postgresql.open(f'pg://{dbuser}:{dbpass}@{dbhost}:{dbport}/{db}'
-    return dbconn
-'''
-Creates a database on a given dbconn object.
-
-@params:
-    dbname: The name of the database to be created. Default: trade
-
-@return:
-    None
-'''
-def create_db(dbname='trade',dbconn):
-    dbconn.execute('CREATE DATABASE '+dbname+' OWNER'+dbuser+';')
-    dbconn.execute('CREATE SCHEMA cryptoml;')
-    dbconn.execute('CREATE TABLE state ( guid, datetime, accountID, nodeID, num_worker, wallet_state, config);')
-    dbconn.execute('CREATE TABLE transaction ( guid, creation_time, source, destination, vol_s, vol_d, complete,complete_time, stateguid );')
-    dbconn.execute('CREATE TABLE exchange_creds ( guid, config_guid, accesskey, accessID );')
-    dbconn.execute('CREATE TABLE node_configuration ( guid, config_guid, strategy, nodeID, start_time );')
-    dbconn.execute('CREATE TABLE configuration ( guid, base_investment, state_guid, nodeconf_guid, accountID, creds_guid, last_update, realise_value, realise_target );')
-    dbconn.execute('CREATE TABLE wallet_state ( guid, exchange, coin, volume, state_guid );')
-
-def accept_new_connections():
-    while True:
-        client, client_address = stateserver.accept()
-        Thread(target=handle_messaging,args=(client,)).start()
-
-def handle_messaging():
 
 class StateManager:
     _threads = {}
-
+    '''
+    Configure the State Manager to accept connections from worker nodes.
+    '''
     def accept_connections(self):
         stateserver.listen(5)
-        ACCEPT_THREAD = Thread(target=accept_new_connections)
+        ACCEPT_THREAD = Thread(target=self.accept_new_connections)
         ACCEPT_THREAD.start()
         ACCEPT_THREAD.join()
         stateserver.close()
+
+    '''
+    Returns a database connection object for the operational database.
+
+    @return:
+        dbconn: a database connection object
+    '''
+    def connect_db(self):
+        dbconn = postgresql.open(f'pg://{dbuser}:{dbpass}@{dbhost}:{dbport}/{db}'
+        return dbconn
+    
+    '''
+    Creates a database on a given dbconn object.
+
+    @params:
+        dbname: The name of the database to be created. Default: trade
+
+    @return:
+        None
+    '''
+    def create_db(self,dbname='trade',dbconn):
+        dbconn.execute('CREATE DATABASE '+dbname+' OWNER'+dbuser+';')
+        dbconn.execute('CREATE SCHEMA cryptoml;')
+        dbconn.execute('CREATE TABLE state ( guid, datetime, accountID, nodeID, num_worker, wallet_state, config);')
+        dbconn.execute('CREATE TABLE transaction ( guid, creation_time, source, destination, vol_s, vol_d, complete,complete_time, stateguid );')
+        dbconn.execute('CREATE TABLE exchange_creds ( guid, config_guid, accesskey, accessID );')
+        dbconn.execute('CREATE TABLE node_configuration ( guid, config_guid, strategy, nodeID, start_time );')
+        dbconn.execute('CREATE TABLE configuration ( guid, base_investment, state_guid, nodeconf_guid, accountID, creds_guid, last_update, realise_value, realise_target );')
+        dbconn.execute('CREATE TABLE wallet_state ( guid, exchange, coin, volume, state_guid );')
+    
+    '''
+    Handler for new connections to the State Manager. Forks off a handler thread for each connection.
+    '''
+    def accept_new_connections(self):
+        while True:
+            client, client_address = stateserver.accept()
+            Thread(target=self.handle_messaging,args=(client,)).start()
+    
+    '''
+    Message handler. Reads messages as they are posted, parses them and performs actions as required. 
+
+    @params:
+        conn: the connection object for the worker node.
+
+    @return:
+        None
+    '''
+    def handle_messaging(self,conn):
+        message = conn.recv(buffersize)
+
+        while True:
+            message = conn.recv(buffersize)
+
+    
 
