c8c251bcda483aa434f67fda9e7708042bbf9ce8
lime_train <me@liamstevens.me>
2021-04-19 22:44:57 +1000

    Added a number of networked messaging functions to class, added threading functionality to support multiple network connections

--- src/state_manager.py
+++ src/state_manager.py
@@ -5,17 +5,41 @@ section of the Confluence.
 '''
 import postgresql
 import os
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+#Load in environment variables
 dbhost = os.environ['DBHOST']
 dbport = os.environ['DBPORT']
 dbuser = os.environ['DBUSER']
 dbcreds = os.environ['DBCREDS']
 dbpass = (open(dbcreds,'r').read())
 
+#Set up localhost to accept connections from clients
+localhost = ''
+port = 31415
+buffersize = 4096
+addr = (localhost, port)
+stateserver = socket(AF_INET, SOCK_STREAM)
+stateserver.bind(addr)
 
-def connect_db(db='postgres'):
+'''
+Returns a database connection object for the operational database.
+
+@return:
+    dbconn: a database connection object
+'''
+def connect_db():
     dbconn = postgresql.open(f'pg://{dbuser}:{dbpass}@{dbhost}:{dbport}/{db}'
     return dbconn
+'''
+Creates a database on a given dbconn object.
 
+@params:
+    dbname: The name of the database to be created. Default: trade
+
+@return:
+    None
+'''
 def create_db(dbname='trade',dbconn):
     dbconn.execute('CREATE DATABASE '+dbname+' OWNER'+dbuser+';')
     dbconn.execute('CREATE SCHEMA cryptoml;')
@@ -26,4 +50,20 @@ def create_db(dbname='trade',dbconn):
     dbconn.execute('CREATE TABLE configuration ( guid, base_investment, state_guid, nodeconf_guid, accountID, creds_guid, last_update, realise_value, realise_target );')
     dbconn.execute('CREATE TABLE wallet_state ( guid, exchange, coin, volume, state_guid );')
 
-def create_schema():
+def accept_new_connections():
+    while True:
+        client, client_address = stateserver.accept()
+        Thread(target=handle_messaging,args=(client,)).start()
+
+def handle_messaging():
+
+class StateManager:
+    _threads = {}
+
+    def accept_connections(self):
+        stateserver.listen(5)
+        ACCEPT_THREAD = Thread(target=accept_new_connections)
+        ACCEPT_THREAD.start()
+        ACCEPT_THREAD.join()
+        stateserver.close()
+
